e27e2311d9dfc88eda577b3d2c5fadd0ca3f30b0
troy0401 <napili.troy01@gmail.com>
2023-05-22 17:03:25 +0800

    minor u.i changes to page name

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Accordion - srtdash</title>
+    <title>Module</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="<?php echo base_url('srtdash-admin-dashboard-master/srtdash/assets/images/icon/favicon.ico')?>">
     <link rel="stylesheet" href="<?php echo base_url('srtdash-admin-dashboard-master/srtdash/assets/css/bootstrap.min.css')?>">
