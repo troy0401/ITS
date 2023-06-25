945b270c73abfd34299642c36875c7be5cccfdff
troy0401 <napili.troy01@gmail.com>
2023-06-25 11:18:44 +0800

    embed video to the system

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -652,7 +652,7 @@
 						'<button type="button" onclick="showRequestModal('+learnExamData[0]['exam_id']+')" class="btn btn-danger btn-lg mb-3">Request Additional attempt <i class="fa fa-send-o"></i></button>' : '<button disabled type="button" class="btn btn-success btn-lg mb-3">Take Exam <i class="fa fa-edit"></i></button>'+
 						'<button type="button" onclick="showRequestModal('+practiceExamData[0]['exam_id']+')" class="btn btn-danger btn-lg mb-3">Request Additional attempt <i class="fa fa-send-o"></i></button>');
 
-						redirect_button=(learnExamData.length>0 ? '<button type="button" onclick="redirectPage(\''+link +'\','+learnExamData[0]['exam_id']+','+subj_id+',\''+name +'\')" class="btn btn-info btn-lg btn-block">View Learning Material <i class="fa fa-eye"></i></button>'+(link2=='' ? '<p>bleh</p>' : '<button type="button" onclick="redirectPage(\''+link2 +'\','+learnExamData[0]['exam_id']+','+subj_id+',\''+name +'\')" class="btn btn-info btn-lg btn-block">View Learning Material <i class="fa fa-eye"></i></button>') : '<button type="button" onclick="redirectPage(\''+link +'\','+practiceExamData[0]['exam_id']+','+subj_id+',\''+name +'\')" class="btn btn-info btn-lg btn-block">View Learning Material <i class="fa fa-eye"></i></button>'+(link2=='' ? '' : '<button type="button" onclick="redirectPage(\''+link2 +'\','+practiceExamData[0]['exam_id']+','+subj_id+',\''+name +'\')" class="btn btn-info btn-lg btn-block">View Learning Material <i class="fa fa-eye"></i></button>'));
+						redirect_button=(learnExamData.length>0 ? '<button type="button" onclick="redirectPage(\''+link +'\','+learnExamData[0]['exam_id']+','+subj_id+',\''+name +'\')" class="btn btn-info btn-lg btn-block">View Learning Material <i class="fa fa-eye"></i></button>'+(link2=='' ? '' : '<button type="button" onclick="redirectPage(\''+link2 +'\','+learnExamData[0]['exam_id']+','+subj_id+',\''+name +'\')" class="btn btn-info btn-lg btn-block">View Learning Material <i class="fa fa-eye"></i></button>') : '<button type="button" onclick="redirectPage(\''+link +'\','+practiceExamData[0]['exam_id']+','+subj_id+',\''+name +'\')" class="btn btn-info btn-lg btn-block">View Learning Material <i class="fa fa-eye"></i></button>'+(link2=='' ? '' : '<button type="button" onclick="redirectPage(\''+link2 +'\','+practiceExamData[0]['exam_id']+','+subj_id+',\''+name +'\')" class="btn btn-info btn-lg btn-block">View Learning Material <i class="fa fa-eye"></i></button>'));
 
 
 						//button=learnExamData.length>0 ? Number(practiceExamData[0]['exam_type']) == 1 ? Number(practiceExamData[0]['exam_status']) == 0 ? Number(practiceExamData[0]['exam_set_trial'])> Number(practiceExamData[0]['exam_trial']) ? active : request_button : inactive: inactive : inactive;
@@ -803,16 +803,18 @@
 
 
 	function redirectPage(page,id,subj_id,name){
-		window.open(page, '_blank');
+		//window.open(page, '_blank');
+		$('#viewLearningModal').modal('show');
+		$('.video-view').empty().apped('<iframe src="'+page+'" frameborder="0" allowfullscreen></iframe>');
 		updateSummativeExamButton(id);
 		ViewSubjStud(subj_id,name,page)
 	}
 
-	function redirectPage1(page,id,subj_id,name){
-		window.open(page, '_blank');
-		updateSummativeExamButton(id);
-		ViewSubjStudCont(subj_id,name,page)
-	}
+	// function redirectPage1(page,id,subj_id,name){
+	// 	window.open(page, '_blank');
+	// 	updateSummativeExamButton(id);
+	// 	ViewSubjStudCont(subj_id,name,page)
+	// }
 
 	function checkSubjSession(subj_id,accnt_id,type){ //lock subtopics if previous subtopics are not yet finished (subtopic 1 only)
 			var result = $.ajax({
--- application/views/student.php
+++ application/views/student.php
@@ -280,6 +280,23 @@
                                     </div>
                                     </div>
 
+                                    <div id="viewLearningModal" class="modal fade bd-example-modal-lg">
+                                    <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Learning Material</h5>
+                                                <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+                                            </div>
+                                                <div class="modal-body video-view">
+                                                    
+											    </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </div>
+
 
                     <!-- Custom Content end -->
                 </div>
