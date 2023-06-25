6e3579024dd18b50e3a06003bff23b847cea9df3
troy0401 <napili.troy01@gmail.com>
2023-06-25 12:07:33 +0800

    resize border

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -806,7 +806,7 @@
 		//window.open(page, '_blank');
 		//alert(page);
 		$('#viewLearningModal').modal('show');
-		$('.video-view').empty().append('<iframe width="500" heigth="500" src="'+page+'"></iframe>');
+		$('.video-view').empty().append('<iframe width="1000" heigth="1000" src="'+page+'"></iframe>');
 		updateSummativeExamButton(id);
 		ViewSubjStud(subj_id,name,page)
 	}
